e67e72cdc414d5967eca8d9b8d7b02925fb679c7
degski <degski@gmail.com>
2020-01-16 15:20:03 -0600

    improvements bugfix

--- include/sax/iostream.hpp
+++ include/sax/iostream.hpp
@@ -28,33 +28,31 @@
 // Escape sequences.
 
 inline std::ostream & nl ( std::ostream & out_ ) { return out_ << '\n'; }
-inline std::wostream & nl ( std::wostream & out_ ) { return out_ << L'\n'; }
-
 inline std::ostream & np ( std::ostream & out_ ) { return out_ << '\f'; }
-inline std::wostream & np ( std::wostream & out_ ) { return out_ << L'\f'; }
-
 inline std::ostream & cr ( std::ostream & out_ ) { return out_ << '\r'; }
-inline std::wostream & cr ( std::wostream & out_ ) { return out_ << L'\r'; }
-
 inline std::ostream & bs ( std::ostream & out_ ) { return out_ << '\b'; }
-inline std::wostream & bs ( std::wostream & out_ ) { return out_ << L'\b'; }
-
 inline std::ostream & sp ( std::ostream & out_ ) { return out_ << ' '; }
-inline std::wostream & sp ( std::wostream & out_ ) { return out_ << L' '; }
-
 inline std::ostream & ab ( std::ostream & out_ ) { return out_ << '\a'; }
-inline std::wostream & ab ( std::wostream & out_ ) { return out_ << L'\a'; }
-
 inline std::ostream & ht ( std::ostream & out_ ) { return out_ << '\t'; }
-inline std::wostream & ht ( std::wostream & out_ ) { return out_ << L'\t'; }
-
 inline std::ostream & vt ( std::ostream & out_ ) { return out_ << '\v'; }
-inline std::wostream & vt ( std::wostream & out_ ) { return out_ << L'\v'; }
 
 // Spaced dash.
 
 inline std::ostream & sd ( std::ostream & out_ ) { return out_ << " - "; }
+#if defined( _UNICODE ) or defined( UNICODE )
+inline std::wostream & nl ( std::wostream & out_ ) { return out_ << L'\n'; }
+inline std::wostream & np ( std::wostream & out_ ) { return out_ << L'\f'; }
+inline std::wostream & cr ( std::wostream & out_ ) { return out_ << L'\r'; }
+inline std::wostream & bs ( std::wostream & out_ ) { return out_ << L'\b'; }
+inline std::wostream & sp ( std::wostream & out_ ) { return out_ << L' '; }
+inline std::wostream & ab ( std::wostream & out_ ) { return out_ << L'\a'; }
+inline std::wostream & ht ( std::wostream & out_ ) { return out_ << L'\t'; }
+inline std::wostream & vt ( std::wostream & out_ ) { return out_ << L'\v'; }
+
+// Spaced dash.
+
 inline std::wostream & sd ( std::wostream & out_ ) { return out_ << L" - "; }
+#endif
 
 // https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
 
@@ -62,94 +60,99 @@ inline std::wostream & sd ( std::wostream & out_ ) { return out_ << L" - "; }
 
 namespace sax {
 
+using string_literal_t = char const *;
+#if defined( _UNICODE ) or defined( UNICODE )
+using wstring_literal_t = wchar_t const *;
+#endif
+
 namespace fg {
 
-inline constexpr char const * black{ "\033[30m" };
-inline constexpr char const * red{ "\033[31m" };
-inline constexpr char const * green{ "\033[32m" };
-inline constexpr char const * yellow{ "\033[33m" };
-inline constexpr char const * blue{ "\033[34m" };
-inline constexpr char const * magenta{ "\033[35m" };
-inline constexpr char const * cyan{ "\033[36m" };
-inline constexpr char const * white{ "\033[37m" };
-inline constexpr char const * default { "\033[39m" };
-inline constexpr char const * bright_black{ "\033[90m" };
-inline constexpr char const * bright_red{ "\033[91m" };
-inline constexpr char const * bright_green{ "\033[92m" };
-inline constexpr char const * bright_yellow{ "\033[93m" };
-inline constexpr char const * bright_blue{ "\033[94m" };
-inline constexpr char const * bright_magenta{ "\033[95m" };
-inline constexpr char const * bright_cyan{ "\033[96m" };
-inline constexpr char const * bright_white{ "\033[97m" };
-inline constexpr char const * bold{ "\033[1m" };
+inline constexpr string_literal_t black{ "\033[30m" };
+inline constexpr string_literal_t red{ "\033[31m" };
+inline constexpr string_literal_t green{ "\033[32m" };
+inline constexpr string_literal_t yellow{ "\033[33m" };
+inline constexpr string_literal_t blue{ "\033[34m" };
+inline constexpr string_literal_t magenta{ "\033[35m" };
+inline constexpr string_literal_t cyan{ "\033[36m" };
+inline constexpr string_literal_t white{ "\033[37m" };
+inline constexpr string_literal_t reset{ "\033[39m" };
+inline constexpr string_literal_t bright_black{ "\033[90m" };
+inline constexpr string_literal_t bright_red{ "\033[91m" };
+inline constexpr string_literal_t bright_green{ "\033[92m" };
+inline constexpr string_literal_t bright_yellow{ "\033[93m" };
+inline constexpr string_literal_t bright_blue{ "\033[94m" };
+inline constexpr string_literal_t bright_magenta{ "\033[95m" };
+inline constexpr string_literal_t bright_cyan{ "\033[96m" };
+inline constexpr string_literal_t bright_white{ "\033[97m" };
+inline constexpr string_literal_t bold{ "\033[1m" };
 #if defined( _UNICODE ) or defined( UNICODE )
-inline constexpr wchar_t const * wblack{ L"\033[30m" };
-inline constexpr wchar_t const * wred{ L"\033[31m" };
-inline constexpr wchar_t const * wgreen{ L"\033[32m" };
-inline constexpr wchar_t const * wyellow{ L"\033[33m" };
-inline constexpr wchar_t const * wblue{ L"\033[34m" };
-inline constexpr wchar_t const * wmagenta{ L"\033[35m" };
-inline constexpr wchar_t const * wcyan{ L"\033[36m" };
-inline constexpr wchar_t const * wwhite{ L"\033[37m" };
-inline constexpr wchar_t const * wdefault{ L"\033[39m" };
-inline constexpr wchar_t const * wbright_black{ L"\033[90m" };
-inline constexpr wchar_t const * wbright_red{ L"\033[91m" };
-inline constexpr wchar_t const * wbright_green{ L"\033[92m" };
-inline constexpr wchar_t const * wbright_yellow{ L"\033[93m" };
-inline constexpr wchar_t const * wbright_blue{ L"\033[94m" };
-inline constexpr wchar_t const * wbright_magenta{ L"\033[95m" };
-inline constexpr wchar_t const * wbright_cyan{ L"\033[96m" };
-inline constexpr wchar_t const * wbright_white{ L"\033[97m" };
-inline constexpr wchar_t const * wbold{ L"\033[1m" };
+inline constexpr wstring_literal_t wblack{ L"\033[30m" };
+inline constexpr wstring_literal_t wred{ L"\033[31m" };
+inline constexpr wstring_literal_t wgreen{ L"\033[32m" };
+inline constexpr wstring_literal_t wyellow{ L"\033[33m" };
+inline constexpr wstring_literal_t wblue{ L"\033[34m" };
+inline constexpr wstring_literal_t wmagenta{ L"\033[35m" };
+inline constexpr wstring_literal_t wcyan{ L"\033[36m" };
+inline constexpr wstring_literal_t wwhite{ L"\033[37m" };
+inline constexpr wstring_literal_t wreset{ L"\033[39m" };
+inline constexpr wstring_literal_t wbright_black{ L"\033[90m" };
+inline constexpr wstring_literal_t wbright_red{ L"\033[91m" };
+inline constexpr wstring_literal_t wbright_green{ L"\033[92m" };
+inline constexpr wstring_literal_t wbright_yellow{ L"\033[93m" };
+inline constexpr wstring_literal_t wbright_blue{ L"\033[94m" };
+inline constexpr wstring_literal_t wbright_magenta{ L"\033[95m" };
+inline constexpr wstring_literal_t wbright_cyan{ L"\033[96m" };
+inline constexpr wstring_literal_t wbright_white{ L"\033[97m" };
+inline constexpr wstring_literal_t wbold{ L"\033[1m" };
 #endif
 } // namespace fg
 
 namespace bg {
 
-inline constexpr char const * black{ "\033[40m" };
-inline constexpr char const * red{ "\033[41m" };
-inline constexpr char const * green{ "\033[42m" };
-inline constexpr char const * yellow{ "\033[43m" };
-inline constexpr char const * blue{ "\033[44m" };
-inline constexpr char const * magenta{ "\033[45m" };
-inline constexpr char const * cyan{ "\033[46m" };
-inline constexpr char const * white{ "\033[47m" };
-inline constexpr char const * default { "\033[49m" };
-inline constexpr char const * bright_black{ "\033[100m" };
-inline constexpr char const * bright_red{ "\033[101m" };
-inline constexpr char const * bright_green{ "\033[102m" };
-inline constexpr char const * bright_yellow{ "\033[103m" };
-inline constexpr char const * bright_blue{ "\033[104m" };
-inline constexpr char const * bright_magenta{ "\033[105m" };
-inline constexpr char const * bright_cyan{ "\033[106m" };
-inline constexpr char const * bright_white{ "\033[107m" };
+inline constexpr string_literal_t black{ "\033[40m" };
+inline constexpr string_literal_t red{ "\033[41m" };
+inline constexpr string_literal_t green{ "\033[42m" };
+inline constexpr string_literal_t yellow{ "\033[43m" };
+inline constexpr string_literal_t blue{ "\033[44m" };
+inline constexpr string_literal_t magenta{ "\033[45m" };
+inline constexpr string_literal_t cyan{ "\033[46m" };
+inline constexpr string_literal_t white{ "\033[47m" };
+inline constexpr string_literal_t reset{ "\033[49m" };
+inline constexpr string_literal_t bright_black{ "\033[100m" };
+inline constexpr string_literal_t bright_red{ "\033[101m" };
+inline constexpr string_literal_t bright_green{ "\033[102m" };
+inline constexpr string_literal_t bright_yellow{ "\033[103m" };
+inline constexpr string_literal_t bright_blue{ "\033[104m" };
+inline constexpr string_literal_t bright_magenta{ "\033[105m" };
+inline constexpr string_literal_t bright_cyan{ "\033[106m" };
+inline constexpr string_literal_t bright_white{ "\033[107m" };
 #if defined( _UNICODE ) or defined( UNICODE )
-inline constexpr wchar_t const * wblack{ L"\033[40m" };
-inline constexpr wchar_t const * wred{ L"\033[41m" };
-inline constexpr wchar_t const * wgreen{ L"\033[42m" };
-inline constexpr wchar_t const * wyellow{ L"\033[43m" };
-inline constexpr wchar_t const * wblue{ L"\033[44m" };
-inline constexpr wchar_t const * wmagenta{ L"\033[45m" };
-inline constexpr wchar_t const * wcyan{ L"\033[46m" };
-inline constexpr wchar_t const * wwhite{ L"\033[47m" };
-inline constexpr wchar_t const * wdefault{ L"\033[49m" };
-inline constexpr wchar_t const * wbright_black{ L"\033[100m" };
-inline constexpr wchar_t const * wbright_red{ L"\033[101m" };
-inline constexpr wchar_t const * wbright_green{ L"\033[102m" };
-inline constexpr wchar_t const * wbright_yellow{ L"\033[103m" };
-inline constexpr wchar_t const * wbright_blue{ L"\033[104m" };
-inline constexpr wchar_t const * wbright_magenta{ L"\033[105m" };
-inline constexpr wchar_t const * wbright_cyan{ L"\033[106m" };
-inline constexpr wchar_t const * wbright_white{ L"\033[107m" };
+inline constexpr wstring_literal_t wblack{ L"\033[40m" };
+inline constexpr wstring_literal_t wred{ L"\033[41m" };
+inline constexpr wstring_literal_t wgreen{ L"\033[42m" };
+inline constexpr wstring_literal_t wyellow{ L"\033[43m" };
+inline constexpr wstring_literal_t wblue{ L"\033[44m" };
+inline constexpr wstring_literal_t wmagenta{ L"\033[45m" };
+inline constexpr wstring_literal_t wcyan{ L"\033[46m" };
+inline constexpr wstring_literal_t wwhite{ L"\033[47m" };
+inline constexpr wstring_literal_t wreset{ L"\033[49m" };
+inline constexpr wstring_literal_t wbright_black{ L"\033[100m" };
+inline constexpr wstring_literal_t wbright_red{ L"\033[101m" };
+inline constexpr wstring_literal_t wbright_green{ L"\033[102m" };
+inline constexpr wstring_literal_t wbright_yellow{ L"\033[103m" };
+inline constexpr wstring_literal_t wbright_blue{ L"\033[104m" };
+inline constexpr wstring_literal_t wbright_magenta{ L"\033[105m" };
+inline constexpr wstring_literal_t wbright_cyan{ L"\033[106m" };
+inline constexpr wstring_literal_t wbright_white{ L"\033[107m" };
 #endif
 } // namespace bg
 
-inline constexpr char const * default_colors{ "\033[0m" };
-inline constexpr char const * invert_colors{ "\033[7m" };
-inline constexpr char const * revert_colors{ "\033[27m" };
+inline constexpr string_literal_t reset{ "\033[0m" };
+inline constexpr string_literal_t invert_colors{ "\033[7m" };
+inline constexpr string_literal_t revert_colors{ "\033[27m" };
 #if defined( _UNICODE ) or defined( UNICODE )
-inline constexpr wchar_t const * wdefault_colors{ L"\033[0m" };
-inline constexpr wchar_t const * winvert_colors{ L"\033[7m" };
-inline constexpr wchar_t const * revert_colors{ L"\033[27m" };
+inline constexpr wstring_literal_t wreset{ L"\033[0m" };
+inline constexpr wstring_literal_t winvert_colors{ L"\033[7m" };
+inline constexpr wstring_literal_t wrevert_colors{ L"\033[27m" };
 #endif
 } // namespace sax
